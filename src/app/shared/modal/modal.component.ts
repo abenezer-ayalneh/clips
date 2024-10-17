7ca22020f07c17fe3093738f1440efb3c4e24d59
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2024-10-17 20:59:05 +0300

    feat: create a portal for the modal to move to the document's body

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, Input, OnInit} from '@angular/core';
 import {ModalService} from "../../services/modal.service";
 import {NgClass} from "@angular/common";
 
@@ -11,12 +11,20 @@ import {NgClass} from "@angular/common";
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
   @Input({required: true}) modalID!: string;
 
   constructor(
     protected readonly modalService: ModalService,
+    private readonly elementRef: ElementRef,
   ) {
+  }
+
+  ngOnInit(): void {
+    // This is like the portal feature in React.js. This line
+    // will move this component to the document's body tag.
+    // That means it will be on the same level as the root app component
+    document.body.appendChild(this.elementRef.nativeElement)
   }
 
   closeModal() {
