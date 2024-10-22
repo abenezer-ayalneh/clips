29514d907fa2ee1ad04374a38ea6a5efa272e96e
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2024-10-22 22:12:29 +0300

    feat: add firebase support and environment variables

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 import {provideEnvironmentNgxMask} from "ngx-mask";
+import {environment} from "../environments/environment";
+import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
+import {getFirestore, provideFirestore} from "@angular/fire/firestore";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideEnvironmentNgxMask()]
+  providers: [
+    provideRouter(routes),
+    provideEnvironmentNgxMask(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
+  ]
 };
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDd4JLHfISrMvmZ8pwYhLwTWOiAoFWjAv4",
+    authDomain: "clips-9bbff.firebaseapp.com",
+    projectId: "clips-9bbff",
+    storageBucket: "clips-9bbff.appspot.com",
+    messagingSenderId: "10961728207",
+    appId: "1:10961728207:web:a6ef346e79ce8774a93dba"
+  }
+}
