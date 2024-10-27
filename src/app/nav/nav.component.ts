3ee210055b2c09b281f138d577d4d161933227ef
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2024-10-27 12:17:03 +0300

    feat: implement logout functionality

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {ModalService} from "../services/modal.service";
 import {AuthService} from "../services/auth.service";
 import {AsyncPipe} from "@angular/common";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-nav',
@@ -16,13 +17,19 @@ export class NavComponent {
 
   constructor(
     private readonly modalService: ModalService,
+    private readonly angularFireAuth: AngularFireAuth,
     protected readonly authService: AuthService,
   ) {
   }
 
-  openModal(event: Event) {
-    event.preventDefault()
+  openModal($event: Event) {
+    $event.preventDefault()
 
     this.modalService.toggleModal('auth')
+  }
+
+  async logout($event: Event) {
+    $event.preventDefault()
+    await this.angularFireAuth.signOut()
   }
 }
