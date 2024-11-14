5f70d9c0822d1360396367b39dd01f253c5e8634
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2024-11-14 22:32:58 +0300

    feat: add the upload route with its component

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-manage',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './manage.component.html',
   styleUrl: './manage.component.scss'
 })
--- src/app/video/video.routes.ts
+++ src/app/video/video.routes.ts
@@ -1,10 +1,18 @@
 import {Routes} from "@angular/router";
 import {ManageComponent} from "./manage/manage.component";
+import {UploadComponent} from "./upload/upload.component";
 
 export const videoRoutes: Routes = [
   {
     path: 'manage',
     component: ManageComponent,
+    data: {
+      authOnly: true
+    }
+  },
+  {
+    path: 'upload',
+    component: UploadComponent,
     data: {
       authOnly: true
     }
