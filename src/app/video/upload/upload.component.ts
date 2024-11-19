55327a078a5e4d62627165fda1916ded647aa1cf
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2024-11-19 21:54:09 +0300

    feat: correctly setup the drop file div

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {EventBlockerDirective} from "../../shared/directives/event-blocker.directive";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-upload',
   standalone: true,
-  imports: [],
+  imports: [
+    EventBlockerDirective,
+    NgClass
+  ],
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.scss'
 })
 export class UploadComponent {
+  isDraggingOverElement = false;
 
+  setIsDraggingOverElement(value: boolean) {
+    this.isDraggingOverElement = value
+  }
+
+  storeFile(event: Event) {
+    this.setIsDraggingOverElement(false)
+
+
+  }
 }
